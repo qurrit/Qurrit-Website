c0f5070c0584be0b1c964a9d0650a54c88f67ef9
qurrit <92171267+qurrit@users.noreply.github.com>
2022-01-31 19:32:02 +0530

    program submitted fix and directed to profile

--- src/Components/Workout_Entry/Program1.jsx
+++ src/Components/Workout_Entry/Program1.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import Workout from "./Workout";
 import WorkoutList from "./WorkoutList";
@@ -8,6 +9,8 @@ import Upload_Image from '../Upload_Image'
 
 const Program1 = ({ user }) => {
     console.log(user)
+    const history = useNavigate();
+    const [submitted, setSubmitted] = useState(false)
     const [programName, setProgramName] = useState("");
     const [trainerName, setTrainerName] = useState("");
     const [duration, setDuration] = useState(0);
@@ -23,6 +26,11 @@ const Program1 = ({ user }) => {
     const [trainerId, setTrainerId] = useState('')
 
     const [imageURL, setImageURL] = useState('')
+
+    const handleProfile = () => {
+        history("/profile")
+        window.location.reload();
+    }
 
     // get trainer id from cookie
     useEffect(() => {
@@ -47,9 +55,9 @@ const Program1 = ({ user }) => {
             alert("Enter Program Name");
         } else if (trainerName === "") {
             alert("Enter Trainer Name ");
-        } else if (duration === 0) {
+        } else if (!duration || duration === 0) {
             alert("Enter Duration");
-        } else if (cost === 0) {
+        } else if (cost === 0 || !cost) {
             alert("Enter Cost");
         } else {
             await fetch("https://qurrit-react.herokuapp.com/api/programs/create", {
@@ -58,8 +66,20 @@ const Program1 = ({ user }) => {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify(programData),
-            });
-            console.log(JSON.stringify(programData))
+            })
+                .then((res) => {
+                    if (res.status === 200) {
+                        setSubmitted(true)
+
+                    } else {
+                        alert('something went wrong, please try again')
+                    }
+                })
+
+                .catch((errs) => {
+                    console.log(errs)
+                })
+
         }
     };
 
@@ -68,99 +88,110 @@ const Program1 = ({ user }) => {
     };
 
     return (
-        <div className='programs'>
-            <div className='program-title'> Create Workout</div>
-            <div className='program-infos'>
-                <div className='program-info'>
-                    <div>
-                        Program Name
+        <secton>
+            {submitted ? <section className=' form-backgroundcolor'>
+                <div className='center-form'>
+                    <div className='form-container'>
+                        <div className='text-color-white center'>
+                            <div>Successfully submitted workout</div>
+                            <button className='default-button background-brandBlue text-color-black' onClick={handleProfile}>go to profile</button></div>
                     </div>
-                    <input
-                        type="text"
-                        onChange={(event) => setProgramName(event.target.value)}
-                        placeholder="Program Name"
-                    />
                 </div>
-                <div className='program-info'>
-                    <div>
-                        Trainer Name
+            </section> :
+                <div className='programs'>
+                    <div className='program-title'> Create Workout</div>
+                    <div className='program-infos'>
+                        <div className='program-info'>
+                            <div>
+                                Program Name
+                            </div>
+                            <input
+                                type="text"
+                                onChange={(event) => setProgramName(event.target.value)}
+                                placeholder="Program Name"
+                            />
+                        </div>
+                        <div className='program-info'>
+                            <div>
+                                Trainer Name
+                            </div>
+                            <input
+                                type="text"
+                                onChange={(event) => setTrainerName(event.target.value)}
+                                placeholder="Trainer Name"
+                            />
+                        </div>
+                        <div className='program-info'>
+                            <div>
+                                Duration
+                            </div>
+                            <input
+                                type="number"
+                                onChange={(event) => setDuration(event.target.value)}
+                                placeholder="Duration"
+                            />
+                        </div>
+                        <div className='program-info'>
+                            <div>
+                                Cost
+                            </div>
+                            <input
+                                type="number"
+                                onChange={(event) => setCost(event.target.value)}
+                                placeholder="In INR"
+                            />
+                        </div>
+                        <div className='program-info'>
+                            <div>
+                                Upload Image for Workout
+                            </div>
+                            <Upload_Image submitImageUrl={setImageURL} />
+                        </div>
                     </div>
-                    <input
-                        type="text"
-                        onChange={(event) => setTrainerName(event.target.value)}
-                        placeholder="Trainer Name"
-                    />
-                </div>
-                <div className='program-info'>
-                    <div>
-                        Duration
-                    </div>
-                    <input
-                        type="number"
-                        onChange={(event) => setDuration(event.target.value)}
-                        placeholder="Duration"
-                    />
-                </div>
-                <div className='program-info'>
-                    <div>
-                        Cost
-                    </div>
-                    <input
-                        type="number"
-                        onChange={(event) => setCost(event.target.value)}
-                        placeholder="In INR"
-                    />
-                </div>
-                <div className='program-info'>
-                    <div>
-                        Upload Image for Workout
-                    </div>
-                    <Upload_Image submitImageUrl={setImageURL} />
-                </div>
-            </div>
 
 
-            {showWorkout ? (
-                <div>
-                    <Workout
-                        submitWorkout={handleWorkoutList}
-                        handleShowWorkout={setShowWorkout}
-                    />
-                </div>
-            ) : (
-                <div>
-                    <button className='add-workout-button margin-left-20px' onClick={() => setShowWorkout(true)}> Add Workout </button>
-                </div>
-            )}
-
-
-            <div className='submitted-program'>
-                <div className='submitted-program-title margin-bottom-20px'>Your Submitted Workout</div>
-                <div className='submit-program-button-position'>
-                    <button className='submit-program-button' onClick={handleSubmitProgram}>Submit Program</button>
-                </div>
-                <div>
-                    {showEditProgram ? null : (
-                        <button className='add-workout-button margin-left-20px' onClick={() => setShowEditProgram(true)}>Edit Program</button>
+                    {showWorkout ? (
+                        <div>
+                            <Workout
+                                submitWorkout={handleWorkoutList}
+                                handleShowWorkout={setShowWorkout}
+                            />
+                        </div>
+                    ) : (
+                        <div>
+                            <button className='add-workout-button margin-left-20px' onClick={() => setShowWorkout(true)}> Add Workout </button>
+                        </div>
                     )}
-                </div>
-                <div>
-                    {showEditProgram ? null : <WorkoutList workoutTable={workoutList} />}
-                </div>
 
 
-                <div>
-                    {showEditProgram ? (
-                        <EditProgram
-                            showEditProgram={setShowEditProgram}
-                            workoutData={workoutList}
-                            setWorkoutList={setWorkoutList}
+                    <div className='submitted-program'>
+                        <div className='submitted-program-title margin-bottom-20px'>Your Submitted Workout</div>
+                        <div className='submit-program-button-position'>
+                            <button className='submit-program-button' onClick={handleSubmitProgram}>Submit Program</button>
+                        </div>
+                        <div>
+                            {showEditProgram ? null : (
+                                <button className='add-workout-button margin-left-20px' onClick={() => setShowEditProgram(true)}>Edit Program</button>
+                            )}
+                        </div>
+                        <div>
+                            {showEditProgram ? null : <WorkoutList workoutTable={workoutList} />}
+                        </div>
 
-                        />
-                    ) : null}
-                </div>
-            </div>
-        </div>
+
+                        <div>
+                            {showEditProgram ? (
+                                <EditProgram
+                                    showEditProgram={setShowEditProgram}
+                                    workoutData={workoutList}
+                                    setWorkoutList={setWorkoutList}
+
+                                />
+                            ) : null}
+                        </div>
+                    </div>
+                </div>}
+        </secton>
     );
 };
 
